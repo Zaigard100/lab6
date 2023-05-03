d2926afea95957cabf1c0477af95583d6ca31cd2
Zaigard100 <70594485+Zaigard100@users.noreply.github.com>
2023-05-03 20:06:33 +0000

    03.05.2023 23:06

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "classes.h"
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
 
-using std::string;
 using std::cin;
+using std::string;
 
 int main() {
   PeopleLinkedList lst;
@@ -12,19 +12,19 @@ int main() {
   string file;
 
   cout << "Из какого файла читать данные:\n";
-  
-  getline(cin,file);
-  
-  if(file == "")
+
+  getline(cin, file);
+
+  if (file == "")
     file = "list.txt";
   std::ifstream fin(file);
-  do{
-    getline(fin,enter);
-    if(enter!="")
+  do {
+    getline(fin, enter);
+    if (enter != "")
       lst.push_back(enter);
-  }while(enter!="");
-  
-  cout<<"\n\n";
-  
+  } while (enter != "");
+
+  cout << "\n\n";
+
   return 0;
 }
